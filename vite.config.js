47718ee15f4fe34111d2e8345b7167b9882c95d5
ashatheo <ashatheo@student.21-school.ru>
2025-11-28 16:49:20 +0500

    gitpages v 1.221

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    open: true, // Автоматически откроет браузер при запуске
-  }
+  base: '/ezcryptotips/', 
 });
